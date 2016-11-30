8d89aebd4d3a5b057c198395aa3c5b3fbdb85714
makahoshi <ma0282a@student.american.edu>
2016-11-30 18:46:37 -0500

    adding the overrides

--- java/src/bot/MinimalAIClient.java
+++ java/src/bot/MinimalAIClient.java
@@ -74,3 +74,40 @@ public class MinimalAIClient implements BWAPIEventListener {
                         }
                 }
         }
+        @Override
+        public void keyPressed(int keyCode) {}
+        @Override
+        public void matchEnd(boolean winner) {}
+        @Override
+        public void sendText(String text) {}
+        @Override
+        public void receiveText(String text) {}
+        @Override
+        public void nukeDetect(Position p) {}
+        @Override
+        public void nukeDetect() {}
+        @Override
+        public void playerLeft(int playerID) {}
+        @Override
+        public void unitCreate(int unitID) {}
+        @Override
+        public void unitDestroy(int unitID) {}
+        @Override
+        public void unitDiscover(int unitID) {}
+        @Override
+        public void unitEvade(int unitID) {}
+        @Override
+        public void unitHide(int unitID) {}
+        @Override
+        public void unitMorph(int unitID) {}
+        @Override
+        public void unitShow(int unitID) {}
+        @Override
+        public void unitRenegade(int unitID) {}
+        @Override
+        public void saveGame(String gameName) {}
+        @Override
+        public void unitComplete(int unitID) {}
+        @Override
+        public void playerDropped(int playerID) {}
+}
